a055a8660ba75d57458ae77b69577f0f7b2cc7de
guidoves <guido.vespignani@gmail.com>
2017-09-11 22:13:36 -0300

    commit

---
+++ ClaseCuatro/MarcaDeAgua.php
@@ -0,0 +1,28 @@
+<?php
+
+$destino = "subida/".$_FILES["archivo"]["name"];
+move_uploaded_file($_FILES["archivo"]["tmp_name"], $destino); 
+
+//$im = imagecreatefrompng("fotoUno.png");
+//$estampa = imagecreatefrompng('fotoDos.png');
+$im = imagecreatefrompng("subida/descarga.png");
+$estampa = imagecreatefrompng("images.png");
+
+
+// Establecer los márgenes para la estampa y obtener el alto/ancho de la imagen de la estampa
+$margen_dcho = 10;
+$margen_inf = 10;
+$sx = imagesx($estampa);
+$sy = imagesy($estampa);
+
+// Copiar la imagen de la estampa sobre nuestra foto usando los índices de márgen y el
+// ancho de la foto para calcular la posición de la estampa. 
+imagecopy($im, $estampa, imagesx($im) - $sx - $margen_dcho, imagesy($im) - $sy - $margen_inf, 0, 0, imagesx($estampa), imagesy($estampa));
+
+// Imprimir y liberar memoria
+header('Content-type: image/png');
+imagepng($im);
+imagedestroy($im);
+
+
+?>
