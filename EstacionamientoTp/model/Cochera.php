07f9b7ef66cfb7c80a83176553f99e0a3b04e788
Guido <guido.vespignani@gmail.com>
2017-11-04 23:25:52 -0300

    commit

--- EstacionamientoTp/model/Cochera.php
+++ EstacionamientoTp/model/Cochera.php
@@ -8,6 +8,61 @@ class Cochera{
     public $especial;
     public $disponible;
 
+    public function Alta(){
+        
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+        $consulta =$objetoAccesoDato->RetornarConsulta("INSERT into cochera (idVehiculo,especial,disponible)values('$this->idVehiculo','$this->especial','$this->disponible')");
+        $consulta->execute();
+        return $objetoAccesoDato->RetornarUltimoIdInsertado();
+                    
+    }
+        
+    public function Baja(){
+        
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+        $consulta =$objetoAccesoDato->RetornarConsulta("
+        delete 
+        from cochera 				
+        WHERE id=:id");	
+        $consulta->bindValue(':id',$this->id, PDO::PARAM_INT);		
+        $consulta->execute();
+        return $consulta->rowCount();
+    }
+        
+    public function Modificar(){
+
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso();
+        $consulta = $objetoAccesoDato->RetornarConsulta("UPDATE cochera
+        set idVehiculo=:idVehiculo,
+        especial=:especial,
+        disponible=:disponible
+        WHERE id=:id");
+        $consulta->bindValue(':id',$this->id,PDO::PARAM_INT);
+        $consulta->bindValue(':idVehiculo',$this->idVehiculo,PDO::PARAM_STR);
+        $consulta->bindValue(':especial',$this->especial,PDO::PARAM_STR);
+        $consulta->bindValue(':disponible',$this->disponible,PDO::PARAM_STR);
+        return $consulta->execute();
+    }
+        
+    public static function BajaPorId($id){
+                
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+        $consulta =$objetoAccesoDato->RetornarConsulta("
+        delete 
+        from cochera 				
+        WHERE id=:id");	
+        $consulta->bindValue(':id',$id, PDO::PARAM_INT);		
+        $consulta->execute();
+        return $consulta->rowCount();
+    }
+        
+    public static function Listar(){
+
+        $objetoAccesoDato = AccesoDatos::dameUnObjetoAcceso(); 
+        $consulta =$objetoAccesoDato->RetornarConsulta("select * from cochera");
+        $consulta->execute();			
+        return $consulta->fetchAll(PDO::FETCH_CLASS,"Cochera");
+    }
 
 
 }
